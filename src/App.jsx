6d198e313445884d73410493582f1842614b2708
Ronal <yepezr.atcode@gmail.com>
2025-08-24 10:20:02 -0500

    new config icons and folder components NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-
 import './App.css'
+import NavBar from './components/NavBar';
 
 function App() {
-  
   return (
-    <>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+      <NavBar />
   )
 }
 
-export default App
+export default App;
